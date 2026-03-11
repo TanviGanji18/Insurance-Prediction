f2bce8842e4f47d4e34c84bd1c0c9d86a5c681ef
TanviGanji18 <ganjijaanu18@gmail.com>
2026-03-11 13:51:09 +0530

    initial commit

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -1,18 +1,23 @@
-#1.Load training and testing data
-#2. Scale the traing data
-#3.Save scaled data into processed folder
+# 1. Load Training and Testing data
+# 2. Scale the training data 
+# 3. Save scaled data into processed folder
+
+from turtle import pd
+import pandas as pd
+import numpy as np
 
 from sklearn.preprocessing import StandardScaler
-import pandas as pd
-import pickle
-from data_preprocessing import load_and_split_data
+import pickle 
 
-X_train, X_test, y_train, y_test = load_and_split_data()
+from data_preprocessing import load_and_Split_data
+X_train,X_test,y_train,y_test=load_and_Split_data()
 
 scaler=StandardScaler()
 
 X_train_scaled=scaler.fit_transform(X_train)
-X_test_scaled=scaler.transform(X_test)
+X_test_scaled=scaler.transform(X_test)  
+
+# 3. Save scaled data into processed folder
 
 pd.DataFrame(X_train_scaled).to_csv("../data/processed/X_train.csv",index=False)
 pd.DataFrame(X_test_scaled).to_csv("../data/processed/X_test.csv",index=False)
@@ -22,5 +27,4 @@ pd.DataFrame(y_test).to_csv("../data/processed/y_test.csv",index=False)
 with open("../artifacts/scaler.pkl","wb") as f:
     pickle.dump(scaler,f)
 
-print("Successfully saved your scaled file")
-
+print("Succesfully Scaled and Saved the data")
--- src/prediction.py
+++ src/prediction.py
@@ -1,19 +1,25 @@
-# 1. load scaler.pkl and model.pkl files
-# 2. create a function to predict
-
+# 1. Load scaler.pkl and model.pkl files
+# 2. Create a function to predict
 
 import pickle
 import numpy as np
 
 class Insurance_Prediction:
-    def __init__(self):
-        with open("../artifacts/scaler.pkl","rb") as f:
-            self.scaler=pickle.load(f)
-        with open("../artifacts/model.pkl","rb") as f:
-            self.model=pickle.load(f)
 
-    def prediction(self,Age, Annual_Income_LPA, Policy_Term_Years, Sum_Assured_Lakhs):
-        input=np.array([[Age, Annual_Income_LPA, Policy_Term_Years, Sum_Assured_Lakhs]])
-        scaled_input=self.scaler.transform(input)
-        result=self.model.predict(scaled_input)
+    def __init__(self):
+
+        with open("artifacts/scaler.pkl", "rb") as f:
+            self.scaler = pickle.load(f)
+
+        with open("artifacts/model.pkl", "rb") as f:
+            self.model = pickle.load(f)
+
+    def prediction(self, Age, Annual_Income_LPA, Policy_Term_Years, Sum_Assured_Lakhs):
+
+        input = np.array([[Age, Annual_Income_LPA, Policy_Term_Years, Sum_Assured_Lakhs]])
+
+        input_scaled = self.scaler.transform(input)
+
+        result = self.model.predict(input_scaled)
+
         return result[0]
