05fdc01dd176b7cb826e25caebf858c9048a8ec5
TanviGanji18 <ganjijaanu18@gmail.com>
2026-03-11 11:54:10 +0530

    initial commit

---
+++ src/feature_engineering.py
@@ -0,0 +1,26 @@
+#1.Load training and testing data
+#2. Scale the traing data
+#3.Save scaled data into processed folder
+
+from sklearn.preprocessing import StandardScaler
+import pandas as pd
+import pickle
+from data_preprocessing import load_and_split_data
+
+X_train, X_test, y_train, y_test = load_and_split_data()
+
+scaler=StandardScaler()
+
+X_train_scaled=scaler.fit_transform(X_train)
+X_test_scaled=scaler.transform(X_test)
+
+pd.DataFrame(X_train_scaled).to_csv("../data/processed/X_train.csv",index=False)
+pd.DataFrame(X_test_scaled).to_csv("../data/processed/X_test.csv",index=False)
+pd.DataFrame(y_train).to_csv("../data/processed/y_train.csv",index=False)
+pd.DataFrame(y_test).to_csv("../data/processed/y_test.csv",index=False)
+
+with open("../artifacts/scaler.pkl","wb") as f:
+    pickle.dump(scaler,f)
+
+print("Successfully saved your scaled file")
+
